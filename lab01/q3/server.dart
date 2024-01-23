104953b9e0237d83c48dbbd4371c57f528c6dc23
testgithubtiwari <rt936649@gmail.com>
2024-01-23 13:36:55 +0530

    change pyrhon code to dart

---
+++ lab01/q3/server.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'dart:convert';
+
+void startServer() async {
+  ServerSocket serverSocket = await ServerSocket.bind('127.0.0.1', 12346);
+  print('Server listening on 127.0.0.1:12346');
+
+  await for (Socket clientSocket in serverSocket) {
+    print(
+        'Connection from ${clientSocket.remoteAddress}:${clientSocket.remotePort}');
+
+    List<int> data = await clientSocket.first;
+    String clientMessage = utf8.decode(data);
+
+    clientSocket.write(clientMessage);
+
+    // Close the connection
+    clientSocket.close();
+    serverSocket.close();
+  }
+}
+
+void main() {
+  startServer();
+}
